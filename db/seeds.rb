907e52d7cbeacebb806795b84815dd3844b5d7c5
jvpelai <jv.pelai@gmail.com>
2020-08-06 18:33:09 -0300

    airport/flights migrations and relations

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,17 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.fir
+
+airports = ['SFO', 'NYC', 'STL', 'AST','CGO']
+
+airports.each do |code|
+  Airport.create(aptcode: code)
+end
+
+i = airports.length
+while i > 1 do
+  Flight.create(from_airport_id: i,to_airport_id: i-1, departure_time: Time.now.strftime("%m/%d/%Y/%H"),duration: rand(2..8))
+  i-=1 
+end
+Flight.create(from_airport_id: 1,to_airport_id: 5, departure_time: Time.now.strftime("%m/%d/%Y/%H"),duration: rand(2..8))
