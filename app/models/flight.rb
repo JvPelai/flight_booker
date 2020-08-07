562ff59d64a9c1740d701ff7097a84eff985e97a
jvpelai <jv.pelai@gmail.com>
2020-08-06 23:36:46 -0300

    search form

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,12 @@
 class Flight < ApplicationRecord
   belongs_to :from_airport, class_name: :Airport 
   belongs_to :to_airport, class_name: :Airport
+
+  scope :search_month, -> (m) {where("cast(strftime('%m', departure_time) as int) = ?", m) }
+
+
+  def flight_date_formatted
+    departure_time.strftime("%m/%d/%Y")
+  end
+
 end
